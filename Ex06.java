fd156fdb23319726bee40a3451a6837eb11488f1
Eduardo Alves e Santos <83457129+eduardoalvese13@users.noreply.github.com>
2024-03-20 22:09:10 -0300

    🟣 Exercício 06 - Estrutura de Repetição
    O conteúdo corresponde ao Exercício 06 da lista intitulada "Estrutura de Repetição" em Python, acessível através do link https://wiki.python.org.br/EstruturaDeRepeticao. No entanto, foi implementado utilizando a linguagem JAVA, conforme instruído pelo instrutor durante a aula.

---
+++ Ex06.java
@@ -0,0 +1,14 @@
+public class Ex06 {
+  public static void main(String[] args) {
+
+    for (int i = 1; i <= 20; i++) {
+      System.out.println(i);
+    }
+
+    System.out.println("--------------------");
+    
+    for (int i = 1; i <= 20; i++) {
+      System.out.print(i+", ");
+    }
+  }
+}
