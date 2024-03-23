62b2c3594da4c37cb76826148ba6f31800fc77bb
Eduardo Alves e Santos <83457129+eduardoalvese13@users.noreply.github.com>
2024-03-22 23:01:23 -0300

    🟣 Exercício 22 - Estrutura de Repetição
    O conteúdo anteriormente corresponde ao Exercício 22 da lista intitulada "Estrutura de Repetição" em Python, acessível através do link https://wiki.python.org.br/EstruturaDeRepeticao. No entanto, foi implementado utilizando a linguagem JAVA, conforme instruído pelo instrutor durante a aula.

---
+++ Ex22.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Ex22{
+  public static void main(String[] args) {
+
+    Scanner teclado = new Scanner(System.in);
+
+    int primo, divisores = 0;
+
+    System.out.println("Digite o número a ser consultado: ");
+    primo = teclado.nextInt();
+    teclado.close();
+
+    for (int i = primo; i >= 1; i--) {
+      if (primo % i == 0) {
+        divisores = divisores + 1;
+      }
+    }
+    if (divisores == 2) {
+      System.out.println("O número " + primo + " é primo.");
+    } else {
+      System.out.println("O número " + primo + " não é primo. Ele possui "+divisores+" divisores, sendo: ");
+
+      for (int i = primo; i >= 1; i--) {
+        if (primo % i == 0) {
+          System.out.print(i + " ");
+        }
+      };
+    }
+
+  }
+
+}
